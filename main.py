67a0740528ffd8c9dc8925c1d93987ca92cc7ea7
chris-adam <31113062+chris-adam@users.noreply.github.com>
2020-03-28 11:58:46 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ import matplotlib.pyplot as plt
 
 
 POPULATION = 100
-PARTICLE_RADIUS = 10
-HEIGTH = 300
-WIDTH = 400
+PARTICLE_RADIUS = 20
+HEIGTH = 400
+WIDTH = 900
 TRANSMISSION_PROBABILITY = 0.4
 HOSPITAL_CAPACITY = POPULATION*0.2
-INCUBATION_PERIOD = 200
+INCUBATION_PERIOD = 500
+PROTECTION = 2  # 0: nobody, 1: doctors, 2: doctors and patients, 3: doctors and infected, 4: everybody
+PROTECTION_EFFICIENCY = 0.7
 
 
 def dist(x1, y1, x2=0, y2=0):
@@ -30,10 +32,13 @@ class ParticleSystem(object):
             else:
                 self.lst_particles.append(Particle())
 
-        self.hospital = Hospital(capacity=HOSPITAL_CAPACITY, radius=100)
+        self.hospital = Hospital(capacity=HOSPITAL_CAPACITY, radius=200)
 
         self.stats = {"healthy": [], "infected": [], "cured": [], "dead": []}
+        t.tracer(0, 0)
+        t.ht()
         self.run()
+        t.done()
 
     def run(self):
         while sum([part.is_contagious() for part in self.lst_particles]) > 0:
@@ -42,27 +47,31 @@ class ParticleSystem(object):
 
             for i in range(len(self.lst_particles)):
                 part1 = self.lst_particles[i]
-                part1.update()
+                if part1.alive:
+                    part1.update()
                 for j in range(i+1, len(self.lst_particles)):
                     part2 = self.lst_particles[j]
                     if part1.alive and part2. alive and part1.check_collisions(part2):
                         ParticleSystem.collision(part1, part2)
                         if (part1.is_contagious() or part2.is_contagious()) \
-                                and random.random() <= TRANSMISSION_PROBABILITY:
+                                and random.random() <= TRANSMISSION_PROBABILITY \
+                                * (1-PROTECTION_EFFICIENCY*max(part1.protection, part2.protection)):
                             part1.time_since_infected = max(0, part1.time_since_infected)
                             part2.time_since_infected = max(0, part2.time_since_infected)
 
                 # Hospital and stuff
-                if part1.is_contagious() and part1.time_since_infected > INCUBATION_PERIOD:
+                if part1.is_sick():
                     if dist(*part1.pos) > self.hospital.radius:
                         if part1.hospital or self.hospital.capacity > self.hospital.patients:
                             part1.dir = (degrees(atan2(part1.pos[1], part1.pos[0]))+180) % 360
                             self.hospital.patients += 1
                     elif not part1.hospital and self.hospital.capacity > self.hospital.patients:
                         part1.hospital = True
-                if not part1.is_contagious() and part1.job != "doc" and dist(*part1.pos) < self.hospital.radius \
-                        and part1.time_since_infected >= INCUBATION_PERIOD:
+                if not part1.is_sick() and part1.job != "doc" and dist(*part1.pos) < self.hospital.radius:
                     part1.dir = degrees(atan2(part1.pos[1], part1.pos[0])) % 360
+
+                # protection setup
+                part1.set_protection()
 
                 if part1.cured:
                     cured += 1
@@ -104,8 +113,11 @@ class ParticleSystem(object):
             t.goto(part.pos)
             t.down()
             if part.hospital:
-                t.color("yellow")
-                t.dot(PARTICLE_RADIUS+5)
+                t.color("dark orange")
+                t.dot(PARTICLE_RADIUS+7)
+            if part.protection == 1:
+                t.color("green")
+                t.dot(PARTICLE_RADIUS+4)
             t.color(part.color)
             t.dot(PARTICLE_RADIUS)
         t.update()
@@ -146,37 +158,48 @@ class Particle(object):
         self.time_since_infected = infected
         self.pos = (random.randint(-WIDTH, WIDTH), random.randint(-HEIGTH, HEIGTH))
         self.dir = random.randint(0, 360)
-        self.velocity = random.random()*2+1.5
+        self.velocity = random.random()*3+2
         self.color = ""
         self.set_color()
+        self.protection = 0
+        self.set_protection()
+
+    def set_protection(self):
+        if PROTECTION == 4 \
+                or (PROTECTION == 3 and (self.job == "doc" or self.is_sick())) \
+                or (PROTECTION == 2 and (self.job == "doc" or self.hospital)) \
+                or (PROTECTION == 1 and self.job == "doc"):
+            self.protection = 1
+        else:
+            self.protection = 0
 
     def death(self):
         # Random probability, if under this probability, subject dies
         random_probability = random.random()*100
         # Compute the probabilities of death for each condition
         if 10 <= self.age <= 19:
-            if random_probability <= 0.02:
+            if random_probability <= 0.02*3:
                 self.alive = False
         elif 20 <= self.age <= 29:
-            if random_probability <= 0.09:
+            if random_probability <= 0.09*3:
                 self.alive = False
         elif 30 <= self.age <= 39:
-            if random_probability <= 0.18:
+            if random_probability <= 0.18*3:
                 self.alive = False
         elif 40 <= self.age <= 49:
-            if random_probability <= 0.4:
+            if random_probability <= 0.4*3:
                 self.alive = False
         elif 50 <= self.age <= 59:
-            if random_probability <= 1.3:
+            if random_probability <= 1.3*3:
                 self.alive = False
         elif 60 <= self.age <= 69:
-            if random_probability <= 4.6:
+            if random_probability <= 4.6*3:
                 self.alive = False
         elif 70 <= self.age <= 79:
-            if random_probability <= 9.8:
+            if random_probability <= 9.8*3:
                 self.alive = False
         elif self.age >= 80:
-            if random_probability <= 18:
+            if random_probability <= 18*3:
                 self.alive = False
 
         if not self.alive:
@@ -184,7 +207,7 @@ class Particle(object):
 
     def set_color(self):
         if not self.alive:
-            self.color = "purple"
+            self.color = "light gray"
         elif self.cured:
             self.color = "green"
         elif self.time_since_infected >= 0:
@@ -201,15 +224,12 @@ class Particle(object):
         if self.is_contagious():
             self.time_since_infected += 1
 
-            lower_limit = 2.5
-            upper_limit = 4
+            lower_limit = 3
+            upper_limit = 10
             if self.hospital and \
-                    log10(self.time_since_infected) > lower_limit+random.random()*(upper_limit-lower_limit):
+                    log10(self.time_since_infected) > lower_limit+random.random()*(upper_limit-lower_limit)-0.1:
                 self.cured = True
                 self.hospital = False
-
-            lower_limit = 3
-            upper_limit = 4.5
             if not self.cured and \
                     log10(self.time_since_infected) > lower_limit+random.random()*(upper_limit-lower_limit):
                 self.death()
@@ -221,6 +241,9 @@ class Particle(object):
 
     def is_contagious(self):
         return self.alive and self.time_since_infected >= 0 and not self.cured
+
+    def is_sick(self):
+        return self.alive and self.time_since_infected >= INCUBATION_PERIOD and not self.cured
 
     def check_boundary_collision(self):
         x, y = self.pos
@@ -234,7 +257,4 @@ class Particle(object):
 
 
 if __name__ == "__main__":
-    t.tracer(0, 0)
-    t.ht()
     sys = ParticleSystem(POPULATION)
-    t.done()
